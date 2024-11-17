875a3b26a56f1f5538f7e18b92019ad26bdbd973
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-11-17 12:48:21 -0300

    recuperando dados do db usuario.

--- AppAuthenticator/app/src/main/java/com/vvieira/appauthenticator/Usuario.kt
+++ AppAuthenticator/app/src/main/java/com/vvieira/appauthenticator/Usuario.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 
 class Usuario(val nome : String, val cpf : String, val email : String, val telefone : String) {
+    override fun toString(): String {
+        return "\n{nome='$nome',\n cpf='$cpf',\n email='$email',\n telefone='$telefone'\n}"
+    }
     private var db = FirebaseFirestore.getInstance()
 
     internal fun salvarDadosUsuario() {
@@ -24,4 +27,38 @@ class Usuario(val nome : String, val cpf : String, val email : String, val telef
             }
     }
 
+    fun recuperarDadosUsuarios() {
+        val usuariosArray = ArrayList<Usuario>()
+        db.collection("Usuarios")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    usuariosArray.add(
+                        Usuario(
+                            document.data["nome"].toString(),
+                            document.data["cpf"].toString(),
+                            document.data["email"].toString(),
+                            document.data["telefone"].toString()
+                        )
+                    )
+                }
+                Log.d("Sucesso", "recuperarUsuarios() - $usuariosArray")
+            }
+    }
+
+    //TODO Validar esse retorno, se faz sentido para carregar informações na tela depois de login
+    fun recuperarDadosUsuario(id: String): Usuario {
+        lateinit var usuario: Usuario
+        db.collection("Usuarios").document(id)
+            .addSnapshotListener { documento, error ->
+                if (documento != null) {
+                    val nome = documento.getString("nome")
+                    val cpf = documento.getString("cpf")
+                    val email = documento.getString("email")
+                    val telefone = documento.getString("telefone")
+                    usuario = Usuario(nome!!, cpf!!, email!!, telefone!!)
+                }
+            }
+        return usuario
+    }
 }
