18d2b60172c088e484448172b1bc61e0b579d821
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2025-02-18 19:52:57 -0300

    reestruturacao do projeto.

--- AppAuthenticator/app/src/main/java/com/vvieira/appauthenticator/domain/usecase/CheckSocialAuthenticUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.vvieira.appauthenticator.domain.usecase
-
-import com.vvieira.appauthenticator.domain.model.LoginModelRequest
-import com.vvieira.appauthenticator.domain.model.ResultRequest
-
-interface CheckSocialAuthenticUseCase {
-    suspend operator fun invoke(user: LoginModelRequest, type: String): ResultRequest?
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# appauthenticator
-Aplicativo de login com autenticação, criado com android com kotlin e firebase.
